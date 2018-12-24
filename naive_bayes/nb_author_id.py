3389bea4475f657464cef309ed8cae3b90649fa4
Shashank Agarwal <shasagar@adobe.com>
2018-12-24 20:50:50 +0530

    adding naive bayes

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,35 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
 
+### create classifier
+clf = GaussianNB()
+t0 = time()
 
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+# 2.378 s
+### fit the classifier on the training features and labels
+
+### use the trained classifier to predict labels for the test features
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+#  0.267 s
+
+correct=0
+for ii in range(0,len(pred)):
+    if (pred[ii] == labels_test[ii]):
+        correct = correct+1
+### calculate and return the accuracy on the test data
+### this is slightly different than the example, 
+### where we just print the accuracy
+### you might need to import an sklearn module
+### print clf.score(features_test, labels_test)
+accuracy = float(correct)/len(pred)
+
+print accuracy
 #########################################################
 
 
